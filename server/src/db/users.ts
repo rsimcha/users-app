159030c1c423d01c7a8d6df52c2211406a94a8d5
rinasimcha <rina.simcha@orchestra.group>
2024-02-14 20:19:07 +0200

    create model and schema

---
+++ server/src/db/users.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  id: { type: Number, required: true },
+  email: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  avatar: { type: String, required: true },
+});
+
+export const UserModel = mongoose.model("User", UserSchema);
