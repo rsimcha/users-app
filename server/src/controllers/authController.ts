f5cd23ad57159b10b7eb1e692d84f22e28256c67
rinasimcha <rina.simcha@orchestra.group>
2024-02-18 12:01:36 +0200

    change signature name for jwt

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 require("dotenv").config();
 
 export const login = async (req: Request, res: Response) => {
-  const accessToken = jwt.sign({}, process.env.ACCESS_TOKEN, {
+  const accessToken = jwt.sign({}, process.env.JWT_SECRET, {
     expiresIn: "30m",
   });
 
